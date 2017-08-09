954013dd2919c32cb8c0fde781e485afb8ebc40f
admin-m <977986310@qq.com>
2017-08-09 14:36:14 +0800

    优化代码

--- series/series.js
+++ series/series.js
@@ -1,13 +1,11 @@
 "use strict"
-
 function findSequence(n) {
-    let fibonacciSeries = [];
-    fibonacciSeries.push(0);
-    fibonacciSeries.push(1);
+    let fibonacciSeries = [0, 1];
     for (let i = 2; i < n; i++) {
         fibonacciSeries.push(fibonacciSeries[i-2]+fibonacciSeries[i-1]);
     }
-    return fibonacciSeries[n-1]
+
+    return "斐波纳契数列第"+n+"数是："+fibonacciSeries[n-1]
 }
 
-console.log(findSequence(10));
+console.log(findSequence(1));
