5f179c1b014504d7dec894e3d905fca93707dc82
admin-m <977986310@qq.com>
2017-08-11 14:38:18 +0800

    优化代码

--- round4/linkedList.js
+++ round4/linkedList.js
@@ -1,9 +1,10 @@
 "use strict";
 
-function linkedList(str) {
+function linkedList(str,val) {
     let newStr = str.split('').filter(item => Number(item));
-    let newString = newStr.filter(item => item % 3 !== 0)
+    let newString = newStr.filter(item => item % val !== 0)
+
     return newString.toString().replace(/,/g,'->');
 }
 
-console.log(linkedList('1->2->3->3->4->5->3'))
+console.log(linkedList('1->2->3->3->4->5->3',3))
