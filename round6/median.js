370c348678dea5946c4a327a6d8647b0210564ab
admin-m <977986310@qq.com>
2017-08-13 14:54:47 +0800

    优化代码

--- round6/median.js
+++ round6/median.js
@@ -12,7 +12,7 @@ function findMedian(array) {
     if (isMedian(array))
         return array[(array.length - 1) / 2]
 
-    return (array[(array.length - 1) / 2] + array[(array.length + 1) / 2])/2;
+    return (array[array.length / 2 - 1] + array[array.length / 2])/2;
 }
 
 console.log(findMedian([5,6,8,4,12,58,21,0,12,4,556,52,1,12,2]))
