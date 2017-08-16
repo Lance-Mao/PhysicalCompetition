4ca1611def051e616b0b43573be16e351c7fa3f3
admin-m <977986310@qq.com>
2017-08-16 14:24:01 +0800

    优化

--- round8/repeatElements.js
+++ round8/repeatElements.js
@@ -1,8 +1,7 @@
 function findRepeatElements(arr) {
 
-    let repeat = new Set(arr.filter(item => (arr.filter(elem => item === elem).length > 1)));
+    return Array.from(new Set(arr.filter(item => (arr.filter(elem => item === elem).length > 1))));
 
-    return repeat;
 }
 
 console.log(findRepeatElements(['q','a','a','b','v','a','c','d','f','c']))
