3d83f75e082782bd9ebf6849f43dcc1cd346587a
admin-m <977986310@qq.com>
2017-08-19 15:16:19 +0800

    优化代码

--- round11/setArr.js
+++ round11/setArr.js
@@ -1,4 +1,5 @@
-function setArra(arr) {
+function setArray(arr) {
     return Array.from(new Set(arr));
 }
-console.log(setArra(['s', 'a', 's', 'b', 'a',1,'1',1]))
+
+console.log(setArray(['s', 'a', 's', 'b', 'a', 1, '1', 1]));
