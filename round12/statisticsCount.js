adb388d605ba247b8344442fff60091ebb28f43f
admin-m <977986310@qq.com>
2017-08-20 22:36:16 +0800

    判断一个字符串中出现次数最多的字符，并统计次数

---
+++ round12/statisticsCount.js
@@ -0,0 +1,19 @@
+function repeatedElementsAndTimes(str) {
+    let sortArr = str.split("").sort();
+    return sortArr.map(item => {
+        return sortArr.filter(elem => item === elem).length + ":" + item
+
+    });
+
+}
+
+function statisticsCount(str) {
+
+    let heavyElementsAndTimes = [...new Set(repeatedElementsAndTimes(str))].sort().reverse();
+
+    let arrayStorage = heavyElementsAndTimes.filter(item => item.split(":")[0] === heavyElementsAndTimes[0].split(":")[0]).sort();
+
+    return "{" + arrayStorage.toString() + "}";
+}
+
+console.log(statisticsCount("asssssadadasdsfaaa"));
